f827743a54baecb68514534c4bfa42635b6e2dc0
Liadrinz <cyang.bupt@foxmail.com>
2025-03-26 00:59:39 +0800

    refactpr and merge `trigger` and `invocation`

--- src/flexygen/mixins/base.py
+++ src/flexygen/mixins/base.py
@@ -1,47 +1,38 @@
+from enum import Enum
+from typing import Union, List, Callable
 from collections import OrderedDict
 from transformers.generation.utils import GenerationMixin
-from .callback import GenerationMixinWithPerIterCallbacks
+from .generation import GenerationMixinWithPerIterCallbacks
 from ..utils import filter_arguments
 
 
-class TriggerInvocationBaseMixin(GenerationMixinWithPerIterCallbacks):
+class InvokeType(Enum):
+    
+    SPLICER = 1
+
+
+class TriggerInjectableMixin(GenerationMixinWithPerIterCallbacks):
     
     def __init__(self):
-        self.trigger_dict = OrderedDict({})
-        self.invocation_dict =  OrderedDict({})
+        self.trigger_dict = {k: OrderedDict({}) for k in InvokeType}
+        self.trigger_options_dict = {k: OrderedDict({}) for k in InvokeType}
     
     def register_tokenizer(self, tokenizer):
         self.tokenizer = tokenizer
         return tokenizer
     
-    def trigger(self, name):
-        def decorate(func):
-            self.trigger_dict[name] = filter_arguments(func)
+    def splicer(self, name, **options):
+        def decorate(func: Callable[..., Union[List[Union[bool, str]], Union[bool, str]]]):
+            self.trigger_dict[InvokeType.SPLICER][name] = filter_arguments(func)
+            self.trigger_options_dict[InvokeType.SPLICER][name] = options
             return func
         return decorate
     
-    def invocation(self, name):
-        def decorate(func):
-            self.invocation_dict[name] = filter_arguments(func)
-            return func
-        return decorate
-    
-    def get_trigger_names(self):
-        return self.trigger_dict.keys()
-    
-    def ask_for_invoke(self, *args, **kwargs):
-        names = self.get_trigger_names()
-        for key in names:
-            if not self.trigger_dict[key](*args, **kwargs):
-                continue
-            invoke_fn = self.invocation_dict.get(key, None)
-            if invoke_fn is None:
-                continue
-            return invoke_fn(*args, **kwargs)
-        return None
+    def get_trigger_names(self, invoke_type: InvokeType):
+        return self.trigger_dict[invoke_type].keys()
     
     @classmethod
-    def wrap(cls, model: GenerationMixin, tokenizer) -> "TriggerInvocationBaseMixin":
+    def wrap(cls, model: GenerationMixin, tokenizer) -> "TriggerInjectableMixin":
         cls.__init__(model)
         
         # TODO: Fix bugs in iterative function replacement
@@ -51,10 +42,8 @@ class TriggerInvocationBaseMixin(GenerationMixinWithPerIterCallbacks):
         
         # Hard-encoded function replacement
         model.register_tokenizer = lambda *args, **kwargs: cls.register_tokenizer(model, *args, **kwargs)
-        model.trigger = lambda *args, **kwargs: cls.trigger(model, *args, **kwargs)
-        model.invocation = lambda *args, **kwargs: cls.invocation(model, *args, **kwargs)
+        model.splicer = lambda *args, **kwargs: cls.splicer(model, *args, **kwargs)
         model.get_trigger_names = lambda *args, **kwargs: cls.get_trigger_names(model, *args, **kwargs)
-        model.ask_for_invoke = lambda *args, **kwargs: cls.ask_for_invoke(model, *args, **kwargs)
         model.on_each_iteration_end = lambda *args, **kwargs: cls.on_each_iteration_end(model, *args, **kwargs)
         model._dola_decoding = lambda *args, **kwargs: cls._dola_decoding(model, *args, **kwargs)
         model._contrastive_search = lambda *args, **kwargs: cls._contrastive_search(model, *args, **kwargs)
@@ -63,6 +52,8 @@ class TriggerInvocationBaseMixin(GenerationMixinWithPerIterCallbacks):
         model._group_beam_search = lambda *args, **kwargs: cls._group_beam_search(model, *args, **kwargs)
         model._constrained_beam_search = lambda *args, **kwargs: cls._constrained_beam_search(model, *args, **kwargs)
         model._assisted_decoding = lambda *args, **kwargs: cls._assisted_decoding(model, *args, **kwargs)
+        model._reset_before_generation = lambda *args, **kwargs: cls._reset_before_generation(model, *args, **kwargs)
+        model.generate = lambda *args, **kwargs: cls.generate(model, *args, **kwargs)
         
         if tokenizer.pad_token is None:
             tokenizer.pad_token = tokenizer.eos_token
